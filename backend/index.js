a087ae1e523b9c5a75309460db3f301999513d11
Kutlu <kutlu.karakoc@akinon.com>
2024-09-11 22:24:46 +0300

    feat: Add product routes to backend index.js

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectToDB } from './config/db.js';
+import productRoutes from './routes/product.route.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.port || 5008;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use('/api/products', productRoutes);
 
 app.listen(PORT, () => {
   connectToDB();
