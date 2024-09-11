e7645681947cb354ff439f45dbe8ca2bff541e1f
Kutlu <kutlu.karakoc@akinon.com>
2024-09-11 22:58:21 +0300

    feat: Implement getProducts function in product controller

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -2,7 +2,24 @@ import Product from "../models/product.model.js";
 
 import { validateProduct } from "../helpers/validation.js";
 
-export const getProducts = async (req, res) => {};
+export const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+
+    res.status(200).json({
+      status: "success",
+      message: "Products retrieved successfully.",
+      total: products.length,
+      data: products,
+    });
+  } catch (error) {
+    console.log("Error getting products:", error.message);
+    res.status(500).json({
+      status: "error",
+      message: "An error occured while getting products.",
+    });
+  }
+};
 
 export const getProductById = async (req, res) => {};
 
