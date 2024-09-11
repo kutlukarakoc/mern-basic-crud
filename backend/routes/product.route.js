7a9a14740be02cef6e57bab087e43fc9030643af
Kutlu <kutlu.karakoc@akinon.com>
2024-09-11 22:23:05 +0300

    feat: Add product routes

---
+++ backend/routes/product.route.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', getProducts);
+router.get('/:id', getProductById);
+router.post('/', createProduct);
+router.put('/:id', updateProduct);
+router.delete('/:id', deleteProduct);
+
+export default router;
