19dd822dc40e8730f26f1ac7a0a52a2b351a61a3
Kutlu <kutlu.karakoc@akinon.com>
2024-09-11 22:54:34 +0300

    feat: Add product validation helper function

---
+++ backend/helpers/validation.js
@@ -0,0 +1,27 @@
+export const validateProduct = (product) => {
+  if (!product) return false;
+
+  const requiredFields = [
+    "brand",
+    "name",
+    "price",
+    "description",
+    "imageUrl",
+    "category.id",
+    "category.name",
+  ];
+
+  for (const field of requiredFields) {
+    const keys = field.split(".");
+    let value = product;
+
+    for (const key of keys) {
+      if (!value[key]) {
+        return false;
+      }
+      value = value[key];
+    }
+  }
+
+  return true;
+};
