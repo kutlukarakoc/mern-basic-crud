cf089aa35cecbbf36e4d64b910534777fe3f4a3c
Kutlu <kutlu.karakoc@akinon.com>
2024-09-11 22:55:04 +0300

    feat: Add createProduct function to product controller

---
+++ backend/controllers/product.controller.js
@@ -0,0 +1,39 @@
+import Product from "../models/product.model.js";
+
+import { validateProduct } from "../helpers/validation.js";
+
+export const getProducts = async (req, res) => {};
+
+export const getProductById = async (req, res) => {};
+
+export const createProduct = async (req, res) => {
+  const product = req.body;
+
+  if (!validateProduct(product)) {
+    return res
+      .status(400)
+      .json({ status: "error", message: "All fields are required." });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+
+    res.status(201).json({
+      status: "success",
+      message: "Product created successfully.",
+      data: newProduct,
+    });
+  } catch (error) {
+    console.log("Error creating product:", error.message);
+    res.status(500).json({
+      status: "error",
+      message: "An error occured while creating product.",
+    });
+  }
+};
+
+export const updateProduct = async (req, res) => {};
+
+export const deleteProduct = async (req, res) => {};
