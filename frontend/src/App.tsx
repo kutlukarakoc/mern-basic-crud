30262f2d21cfa986a7c3224dd616d7515786c774
Kutlu <kutlu.karakoc@akinon.com>
2024-09-12 21:17:28 +0300

    chore: initialized tanstack router

--- frontend/src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div>
-      Hey
-    </div>
-  )
-}
-
-export default App
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,27 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import ReactDOM from 'react-dom/client'
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
 import "./index.css";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
+const router = createRouter({ routeTree });
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </StrictMode>
-);
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const rootElement = document.getElementById("root")!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </StrictMode>
+  );
+}
