1fea343646a22a09a4119b7931c63f48295fed07
Kutlu <kutlu.karakoc@akinon.com>
2024-09-12 23:42:26 +0300

    refactor: Update root route component styling

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -3,11 +3,11 @@ import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>
+      <div className="container mx-auto py-4 flex gap-4">
+        <h3 className="text-lg font-bold">MERN CRUD</h3>
+        <Link to="/">Home</Link>
       </div>
+      <hr />
       <Outlet />
     </>
   ),
