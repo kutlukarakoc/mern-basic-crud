ffc6986740766abbd80c16b4f6bdcfc44b24da40
Kutlu <kutlu.karakoc@akinon.com>
2024-09-12 23:35:28 +0300

    feat: Add ProductsTable component

---
+++ frontend/src/components/ProductsTable.tsx
@@ -0,0 +1,54 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+import type { Products } from "@/types/products";
+import { EditProduct } from "./EditProduct";
+import { DeleteProduct } from "./DeleteProduct";
+
+type ProductsTableProps = {
+  products: Products | undefined;
+};
+
+export const ProductsTable = ({ products }: ProductsTableProps) => {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Brand</TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead className="w-52">Price</TableHead>
+          <TableHead className="text-right">Actions</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {products && products.data ? (
+          products.data.map((product) => (
+            <TableRow key={product._id}>
+              <TableCell>{product.brand}</TableCell>
+              <TableCell>{product.name}</TableCell>
+              <TableCell>${product.price}</TableCell>
+              <TableCell className="text-right w-10">
+                <div className="flex gap-2">
+                  <EditProduct product={product} />
+                  <DeleteProduct product={product} />
+                </div>
+              </TableCell>
+            </TableRow>
+          ))
+        ) : (
+          <TableRow>
+            <TableCell colSpan={24} className="text-center pt-10">
+              No products found
+            </TableCell>
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+  );
+};
