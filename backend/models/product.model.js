a6677fbd378c1748e18a316ab7a91c97062d9958
Kutlu <kutlu.karakoc@akinon.com>
2024-09-12 18:18:45 +0300

    fix: Refactor product model export statement

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -31,13 +31,14 @@ const productSchema = new mongoose.Schema(
         type: String,
         required: true,
       },
-    }
+    },
   },
   {
     timestamps: true,
   }
 );
 
-const Product = mongoose.model("Product", productSchema);
+const Product =
+  mongoose.models.Product || mongoose.model("Product", productSchema);
 
 export default Product;
