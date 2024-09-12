7de294d4c09d15bec25cb4f6e149c58a19a181f6
Kutlu <kutlu.karakoc@akinon.com>
2024-09-12 21:42:57 +0300

    refactor: Update vite.config.ts to add proxy configuration for API requests

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,19 @@
 import path from "path";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import viteReact from "@vitejs/plugin-react";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite(), viteReact()],
+  plugins: [viteReact()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5008",
+      },
     },
   },
 });
