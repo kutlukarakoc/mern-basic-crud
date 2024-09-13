420235e78c790997b341eab57ef93775c772a4a2
Kutlu <kutlu.karakoc@akinon.com>
2024-09-13 21:04:28 +0300

    feat: Add CreateProduct component to Home page

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -3,20 +3,28 @@ import { useQuery } from "@tanstack/react-query";
 import { getProducts } from "@/services/get-products";
 
 import { ProductsTable } from "@/components/ProductsTable";
+import { CreateProduct } from "@/components/CreateProduct";
 
 export const Route = createFileRoute("/")({
   component: Home,
 });
 
-function Home () {
-  const { data, isLoading, isError } = useQuery({ queryKey: ['products'], queryFn: getProducts })
-  
-  if (isLoading) return <div>Loading...</div>
+function Home() {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["products"],
+    queryFn: getProducts,
+  });
 
-  if (isError) return <div>Error</div>
+  if (isLoading) return <div>Loading...</div>;
+
+  if (isError) return <div>Error</div>;
 
   return (
     <div className="container mx-auto mt-10">
+      <div className="flex justify-between mb-10">
+        <h3>My Products</h3>
+        <CreateProduct />
+      </div>
       <ProductsTable products={data} />
     </div>
   );
