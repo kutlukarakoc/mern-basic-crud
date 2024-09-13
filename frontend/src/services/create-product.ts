c3d5b05f4dd350c9b2f50becd78b4318705dc7fc
Kutlu <kutlu.karakoc@akinon.com>
2024-09-14 00:27:40 +0300

    refactor: Update createProduct function to use axios for API request

--- frontend/src/services/create-product.ts
+++ frontend/src/services/create-product.ts
@@ -1,13 +1,15 @@
+import axios from "axios";
+
 import type { ProductFormValues } from "@/schemas/product-form-schema";
 
 export const createProduct = async (product: ProductFormValues) => {
-  const response = await fetch("/api/products", {
+  const response = await axios("/api/products", {
     method: "POST",
     headers: {
-      "Content-Type": "application/json", 
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(product),
+    data: product,
   });
-  
-  return response.json();
+
+  return response.data;
 };
