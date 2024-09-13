075ed97dbb619eef929ef50a820753b6524bc37c
Kutlu <kutlu.karakoc@akinon.com>
2024-09-14 00:11:14 +0300

    refactor: Add generateProductFormDefaults function

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,23 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+import type { Product } from "@/types/product";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const generateProductFormDefaults = (
+  isEditForm: boolean,
+  product: Product | undefined
+) => ({
+  brand: isEditForm ? product!.brand : "",
+  name: isEditForm ? product!.name : "",
+  price: isEditForm ? product!.price : (undefined as unknown as number),
+  description: isEditForm ? product!.description : "",
+  imageUrl: isEditForm ? product!.imageUrl : "",
+  category: {
+    name: isEditForm ? product!.category.name : "",
+    id: isEditForm ? product!.category.id : (undefined as unknown as number),
+  },
+});
