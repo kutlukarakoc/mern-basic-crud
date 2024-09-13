0a1c0602476358e1b879df77ebc457e63d5a8e07
Kutlu <kutlu.karakoc@akinon.com>
2024-09-14 01:21:01 +0300

    refactor: Update API proxy target URL in vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5008",
+        target: "https://mern-basic-crud-server-ashy.vercel.app",
       },
     },
   },
