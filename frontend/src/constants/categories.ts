fe9e61cf9f4bfc559cec874347f8ea5db7a1e6c1
Kutlu <kutlu.karakoc@akinon.com>
2024-09-13 22:23:52 +0300

    feat: Add categories as dummy

---
+++ frontend/src/constants/categories.ts
@@ -0,0 +1,112 @@
+import type { Category } from "@/types/category";
+
+export const categories = [
+  {
+    id: 1,
+    name: "Electronics & Devices",
+  },
+  {
+    id: 2,
+    name: "Books",
+  },
+  {
+    id: 3,
+    name: "Clothing",
+  },
+  {
+    id: 4,
+    name: "Beauty",
+  },
+  {
+    id: 5,
+    name: "Home & Kitchen",
+  },
+  {
+    id: 6,
+    name: "Sports",
+  },
+  {
+    id: 7,
+    name: "Toys",
+  },
+  {
+    id: 8,
+    name: "Automotive",
+  },
+  {
+    id: 9,
+    name: "Music",
+  },
+  {
+    id: 10,
+    name: "Movies",
+  },
+  {
+    id: 11,
+    name: "Health",
+  },
+  {
+    id: 12,
+    name: "Grocery",
+  },
+  {
+    id: 13,
+    name: "Jewelry",
+  },
+  {
+    id: 14,
+    name: "Garden",
+  },
+  {
+    id: 15,
+    name: "Tools",
+  },
+  {
+    id: 16,
+    name: "Pet Supplies",
+  },
+  {
+    id: 17,
+    name: "Baby",
+  },
+  {
+    id: 18,
+    name: "Industrial",
+  },
+  {
+    id: 19,
+    name: "Software",
+  },
+  {
+    id: 20,
+    name: "Office Products",
+  },
+  {
+    id: 21,
+    name: "Arts",
+  },
+  {
+    id: 22,
+    name: "Outdoors",
+  },
+  {
+    id: 23,
+    name: "Handmade",
+  },
+  {
+    id: 24,
+    name: "Collectibles",
+  },
+  {
+    id: 25,
+    name: "Digital Products",
+  },
+  {
+    id: 26,
+    name: "Services",
+  },
+  {
+    id: 27,
+    name: "Other",
+  },
+] satisfies Category[];
