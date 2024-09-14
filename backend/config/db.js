3f8cb3a37a5ea298e245c1f8427a1c39112c4b7a
Kutlu <kutlu.karakoc@akinon.com>
2024-09-14 12:35:19 +0300

    refactor: Remove unnecessary await in connectToDB function

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectToDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_URI);
+    mongoose.connect(process.env.DB_URI);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB", error);
