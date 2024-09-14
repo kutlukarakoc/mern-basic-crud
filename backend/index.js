37ac01eff1a015ce86a3c0c9bd5fae45134159c3
Kutlu <kutlu.karakoc@akinon.com>
2024-09-14 12:19:40 +0300

    Refactor backend/index.js to add CORS support

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,18 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectToDB } from './config/db.js';
 import productRoutes from './routes/product.route.js';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.port || 5008;
+
+app.use(cors({
+  origin: '*',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
 
 app.use(express.json());
 app.use('/api/products', productRoutes);
