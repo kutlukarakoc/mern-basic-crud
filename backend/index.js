14f8381712b32222942b3808e136b9626edb8a24
Kutlu <kutlu.karakoc@akinon.com>
2024-09-14 13:04:07 +0300

    refactor: Update MongoDB connection in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,27 @@
-import express from 'express';
-import dotenv from 'dotenv';
-// import { connectToDB } from './config/db.js';
-import productRoutes from './routes/product.route.js';
-import cors from 'cors';
-import mongoose from 'mongoose';
+import express from "express";
+import dotenv from "dotenv";
+import { connectToDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.port || 5008;
 
+await connectToDB();
+
 const corsOptions = {
-  origin: '*',
+  origin: "*",
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ["GET", "POST", "PUT", "DELETE"],
 };
-app.options('', cors(corsOptions));
+app.options("", cors(corsOptions));
 app.use(cors(corsOptions));
 
 app.use(express.json());
-app.use('/api/products', productRoutes);
+app.use("/api/products", productRoutes);
 
-app.listen(PORT, async () => {
-  try {
-    await mongoose.connect(process.env.DB_URI);
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("Error connecting to MongoDB", error);
-    process.exit(1); // Exit the process if the connection fails
-  }
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
